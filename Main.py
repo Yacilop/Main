4a9ac61ee2694b61cb747eb6b68e65f90a50cacb
Kazakov VO <kazakovvo@gmail.com>
2022-08-23 14:11:11 +0300

    add

--- Main.py
+++ Main.py
@@ -1,14 +1,55 @@
-import random
+import requests
+from selectolax.parser import HTMLParser
+from urllib.parse import unquote
+import json
+from datetime import datetime
 
-digits = [random.randint(0, 25) for count in range(0, 4)]
-aver = sum(digits) / len(digits)
 
-print('Числа: ' + ', '.join(str(digit) for digit in digits))
-print('Среднее арифметическое: ', aver, '\n')
-def factor(x):
-    if x == 0:
-        return 1
-    return factor(x - 1) * x
+def get_json(url):
+    data = {}
+    response = requests.get(url=url)
+    html = response.text
 
-for digit in digits:
-    print('Факториал числа', digit, '\t:', factor(digit))
+    tree = HTMLParser(html)
+    scripts = tree.css("script")
+    for script in scripts:
+        if 'window.__initialData__' in script.text():
+            jsontext = (script.text().split(';')[0].split('=')[1].strip())
+            jsontext = unquote(jsontext[1:-1])
+
+            data = json.loads(jsontext)
+
+            with open("data.json", 'w') as file:
+                json.dump(data, file, ensure_ascii=False)
+    return data
+
+def get_offers(data):
+    offers = []
+    for key in data:
+        if 'bx-single-page' in key:
+            items = data[key]["data"]["catalog"]["items"]
+            for item in items:
+                if "id" in item:
+                    offer = {}
+                    offer['title'] = item['title'].strip().replace('\xa0', '')
+                    offer['price'] = item['priceDetailed']['value']
+                    offer['date'] = datetime.fromtimestamp(item['sortTimeStamp'] / 1000)
+                    offer['url'] = 'https://www.avito.ru' + item['urlPath']
+
+                    offers.append(offer)
+    return offers
+
+def main():
+    url = 'https://www.avito.ru/mtsensk/kvartiry/sdam/na_dlitelnyy_srok-ASgBAgICAkSSA8gQ8AeQUg?cd=1'
+    data = get_json(url)
+    offers = get_offers(data)
+    # with open('data.json', 'r', encoding="utf-8") as file:
+    #     data = json.load(file)
+    #     offers = get_offer(data)
+
+    for offer in offers:
+        print(offer)
+
+
+if __name__ == '__main__':
+    main()
